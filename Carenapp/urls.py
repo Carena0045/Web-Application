18239a7d77af89129ee9de27ee817b3bb22e60cd
JaneMusimbi <janemusimbij@gmail.com>
2023-05-05 20:42:27 +0300

    Web application

---
+++ Carenapp/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path
+from .import  views
+
+
+urlpatterns = [
+ path('',views.home, name="home"),
+ path('contact.html',views.contact, name="contact"),
+ path('about.html',views.about, name="about"),
+ path('services.html', views.services, name="services"),
+ path('nutritionist.html', views.nutritionist, name="nutritionist"),
+ path('training.html', views.training, name="training"),
+ path('register.html', views.register, name="register"),
+ path('login.html', views.login, name="login"),
+ path('logout.html', views.logout, name="logout"),
+ ]
---
+++ Carenapp/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html',{})
+def about(request):
+    return render(request,'about.html',{})
+
+def services(request):
+    return render(request, 'services.html',{}) 
+def nutritionist(request):
+    return render(request, 'nutritionist.html',{}) 
+
+def training(request):
+    return render(request, 'training.html',{})
+def register(request):
+    return render(request, 'register.html',{})    
+def login(request): 
+    return render(request, 'login.html',{})
+def logout(request):
+    return render(request,'logout.html',{})
+                  
+          
+
+def contact(request):
+    if request.method=='POST':
+        Message_name = request.POST[" Message + name"]
+        Message_email=request.POST[" Message + email"]
+        Message_Subject=request.POST[" Message + subject"]
+        umessage=request.POST[" umessage"]
+
+        send_email(
+              Message_name,#users name
+              Message_Subject,# message subject
+              umessage,# messages
+              Message_email, #to email
+              ["carenakhonya22@gmail.com"] # to email
+
+        )
+           
+        return render(request,'contact.html',{" Message_name": Message_name})
+
+    else:
+            return render(request,'contact.html',{})
